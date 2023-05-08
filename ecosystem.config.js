a8bd5735a549256edc57fe7d45241dd7fa069082
tiendev <ltien540@gmail.com>
2023-05-08 13:47:52 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
       host: "103.160.89.20",
       ref: "origin/main",
       repo: "git@github.com:TonyLeeIT/Booking_Dates_Hanbiro.git",
-      path: "/var/www/html",
+      path: "/var/www/html/Booking_Dates_Hanbiro",
       "pre-deploy-local": "",
       "post-deploy":
         "npm install && npm run build && pm2 reload ecosystem.config.js --env production",
